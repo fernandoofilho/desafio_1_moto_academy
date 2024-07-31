a7b798b0d823f91307448ad7d7431d6e32c488e7
fernando <faaf.eng19@uea.edu.br>
2024-07-31 19:33:37 -0400

    definitive version

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+function clearData() {
+   
+    document.getElementById("Bairro-input").value = "";
+    document.getElementById("Cidade-input").value = "";
+    document.getElementById("Estado-input").value = "";
+    document.getElementById("Rua-input").value = "";
+
+}
 async function getData(url) {
   try {
     const response = await fetch(url);
     data = await response.json();
     
     document.getElementById("Bairro-input").value = data.bairro;
-    document.getElementById("Cidade-input").value = data.cidade;
+    document.getElementById("Cidade-input").value = data.localidade;
     document.getElementById("Estado-input").value = data.uf;
-    document.getElementById("Bairro-input").value = data.bairro;
-    document.getElementById("Bairro-input").value = data.bairro;
+    document.getElementById("Rua-input").value = data.logradouro;
+
+    if (!data.uf){
+      alert("CEP não encontrado")
+      clearData()
+    }
 
 } catch (error) {}
 }
+
+
 function events(cep) {
-console.log(cep)
-  if (cep.length === 8) {
-    getData(`https://viacep.com.br/ws/${cep}/json/`);
-  }
+  console.log(cep)
+    if (cep.length === 8) {
+      getData(`https://viacep.com.br/ws/${cep}/json/`);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
